4ac35d1fe83f2ac62351935a07306fa72bd1165e
kanglerian <kanglerian@gmail.com>
2020-12-07 20:50:47 +0700

    upload project shaynavue

---
+++ src/components/WomenShayna.vue
@@ -0,0 +1,119 @@
+<template>
+    <!-- Women Banner Section Begin -->
+    <section class="women-banner spad">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 mt-5">
+
+                    <carousel class="product-slider" :items="3" :nav="false" :dots="false" :autoplay="true"> 
+
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/mickey1.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                                <router-link to="/product">
+                                    <h5>Mickey Baggy</h5>
+                           </router-link>
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/mickey2.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="product.html">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div><div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/mickey3.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="product.html">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div><div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/mickey4.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="product.html">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </carousel>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Women Banner Section End -->
+</template>
+
+
+<script>
+
+import carousel from 'vue-owl-carousel';
+
+export default {
+    name: 'WomenShayna',
+    components:{
+    carousel
+    }
+}
+</script>
+
+<style scoped>
+.product-item {
+    margin-right: 25px;
+}
+</style>
